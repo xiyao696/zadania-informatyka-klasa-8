9e912bb3029c22a0b078a38799a3c5890baec51b
xiyao696 <krystian8222+github@gmail.com>
2023-03-09 19:43:24 +0100

    cwiczenie 4
    ~ do naprawienia

---
+++ zadania temat algorytmy na liczbach naturalnych/cwiczenie 4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+  int l_g, l_u, i, klasa;
+  klasa = 1;
+  for(i = 0; i < 5; i++)
+    {
+        cout << "Uczniowie klasy " << klasa << ": ";
+        klasa = klasa + 1;
+        cin >> l_u;
+        if (l_u % 6 || l_u == 6)
+        {
+            l_g = l_u / 6;
+            cout << "Liczba grup: " << l_g << endl;
+        }
+        else
+        {
+            cout << "Nie utworzono grup z wszystkich graczy" << endl;
+        }
+    }
+  return 0;
+}
